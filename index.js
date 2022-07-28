cd96ab41bd27153b605da66d053e8ff8404b554b
nmanoucheri001 <nmanoucheri001@idtech.com>
2022-07-27 20:11:21 -0700

    hope

---
+++ index.js
@@ -0,0 +1,11 @@
+// Setup basic express server
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
